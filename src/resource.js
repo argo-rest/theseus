779cc2dc691f7b9f197fa7a2f5cf24276d88a7ef
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-09-21 16:05:57 +0100

    Simplification, doc

--- src/resource.js
+++ src/resource.js
@@ -4,55 +4,66 @@ import uriTemplates from 'npm:uri-templates';
 
 var http = new Http;
 
-function fillUriTemplate(params) {
+function fillUriTemplate(params = {}) {
   return function(uri) {
-    return uriTemplates(uri).fillFromObject(params || {});
+    return uriTemplates(uri).fillFromObject(params);
   };
+}
+
+function defProperty(obj, propName, value) {
+  Object.defineProperty(obj, propName, {
+    value: value
+  });
+}
+
+function isDefined(value) {
+  return typeof value !== 'undefined';
 }
 
 
 
 export class Resource {
-  constructor(uri, responsePromise) {
+  constructor(uri, response) {
     if (! uri) {
       throw new Error('Missing required uri argument to Resource');
     }
 
     // uri may be a String or a Promise[String] - flatten to Promise[String]
-    var uriPromise = Promise.resolve(uri);
-    Object.defineProperty(this, 'uri', {
-      value: uriPromise,
-      enumerable: true,
-      configurable: false,
-      writable: false
-    });
+    defProperty(this, 'uri', Promise.resolve(uri));
 
     // Optional content response promise
-    if (typeof responsePromise !== 'undefined') {
-      Object.defineProperty(this, 'responsePromise', {
-        value: responsePromise,
-        enumerable: false,
-        configurable: false,
-        writable: false
-        // FIXME: NOT READABLE?
-      });
-      // FIXME: read-only
+    if (isDefined(response)) {
+      // may be data or promise -- flatten to Promise
+      defProperty(this, 'responsePromise', Promise.resolve(response));
+      // FIXME: NOT READABLE?
     }
+    // FIXME: else read-only, not writable
     // TODO: else lazy GET?
   }
 
 
   /* == HTTP methods == */
 
-  get(params) {
+  /**
+   * @return {Resource}
+   */
+  get(params = {}) {
     // FIXME: must return Resource, not Promise[Resource] - but how do we know it's a Hyper resource?
     return new Resource(this.uri, this.uri.then(uri => http.get(uri, params)));
     // TODO: Any -> Any|Resource recursive deserialiser
   }
+
+  /**
+   * @return {Promise[Any]}
+   */
   post(data)  {
     // FIXME: return Resource or Promise[Any|Resource]? how do we know it's a Hyper resource? is it even?
     return new Resource(this.uri, this.uri.then(uri => http.post(uri, data)));
   }
+
+  /**
+   * @return {Resource}
+   */
   put(data) {
     var putResp = this.uri.then(uri => http.put(uri, data));
     // FIXME: Content of returned Resource is either the server response to
@@ -94,7 +105,7 @@ export class Resource {
 
   /* == Helpers == */
 
-  follow(rel, params) {
+  follow(rel, params = {}) {
     var linkHref = this.getLink(rel).then(l => l.href).then(fillUriTemplate(params));
     // FIXME: substitute params here or later in get? both? default bind param here, allow late binding in GET later?
     return new Resource(linkHref);
