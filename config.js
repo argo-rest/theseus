d72eb6a6d8b52f2fc6f7bc415c6b53fb121bbb74
Oliver Joseph Ash <oliverjash@icloud.com>
2014-11-06 15:53:16 +0100

    Re-shuffle
    New version of jspm.

--- config.js
+++ config.js
@@ -8,37 +8,35 @@ System.config({
 
 System.config({
   "map": {
-    "reqwest": "github:ded/reqwest@^1.1.2",
+    "jquery": "github:components/jquery@^2.1.1",
     "npm:uri-templates": "npm:uri-templates@^0.1.5",
-    "npm:uri-templates@0.1.5": {
+    "reqwest": "github:ded/reqwest@^1.1.2",
+    "github:jspm/nodelibs@0.0.5": {
       "json": "github:systemjs/plugin-json@master"
     },
-    "github:jspm/nodelibs@0.0.3": {
-      "Base64": "npm:Base64@0.2",
-      "inherits": "npm:inherits@^2.0.1",
-      "json": "github:systemjs/plugin-json@master",
-      "ieee754": "npm:ieee754@^1.1.1",
-      "base64-js": "npm:base64-js@0.0"
-    },
     "npm:Base64@0.2.1": {},
-    "npm:inherits@2.0.1": {},
-    "npm:ieee754@1.1.4": {},
     "npm:base64-js@0.0.7": {},
-    "jquery": "github:components/jquery@^2.1.1"
+    "npm:ieee754@1.1.4": {},
+    "npm:inherits@2.0.1": {},
+    "npm:json@9.0.2": {},
+    "npm:uri-templates@0.1.5": {
+      "json": "github:systemjs/plugin-json@master"
+    }
   }
 });
 
 System.config({
   "versions": {
-    "github:ded/reqwest": "1.1.2",
-    "npm:uri-templates": "0.1.5",
-    "github:jspm/nodelibs": "0.0.3",
-    "npm:Base64": "0.2.1",
-    "npm:inherits": "2.0.1",
+    "github:components/jquery": "2.1.1",
+    "github:ded/reqwest": "1.1.5",
+    "github:jspm/nodelibs": "0.0.5",
     "github:systemjs/plugin-json": "master",
-    "npm:ieee754": "1.1.4",
+    "npm:Base64": "0.2.1",
     "npm:base64-js": "0.0.7",
-    "github:components/jquery": "2.1.1"
+    "npm:ieee754": "1.1.4",
+    "npm:inherits": "2.0.1",
+    "npm:json": "9.0.2",
+    "npm:uri-templates": "0.1.5"
   }
 });
 
