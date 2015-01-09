72dbbc897dba6426ceb864c332cb66f510e8ca89
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-01-09 14:54:24 +0000

    Update to latest jspm 0.10.4

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 System.config({
   "paths": {
     "*": "*.js",
+    "theseus/*": "src/*.js",
     "github:*": "jspm_packages/github/*.js",
     "npm:*": "jspm_packages/npm/*.js"
   }
@@ -8,37 +9,33 @@ System.config({
 
 System.config({
   "map": {
-    "reqwest": "github:ded/reqwest@^1.1.2",
-    "uri-templates": "npm:uri-templates@^0.1.5",
+    "jquery": "github:components/jquery@2.1.3",
+    "reqwest": "github:ded/reqwest@1.1.5",
+    "uri-templates": "npm:uri-templates@0.1.5",
+    "github:jspm/nodelibs-path@0.1.0": {
+      "path-browserify": "npm:path-browserify@0.0.0"
+    },
+    "github:jspm/nodelibs-process@0.1.0": {
+      "process": "npm:process@0.10.0"
+    },
+    "github:jspm/nodelibs-util@0.1.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:inherits@2.0.1": {
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:path-browserify@0.0.0": {
+      "process": "github:jspm/nodelibs-process@0.1.0"
+    },
     "npm:uri-templates@0.1.5": {
-      "json": "npm:json@^9.0.2"
+      "path": "github:jspm/nodelibs-path@0.1.0",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0",
+      "util": "github:jspm/nodelibs-util@0.1.0"
     },
-    "github:jspm/nodelibs@0.0.3": {
-      "Base64": "npm:Base64@0.2",
-      "inherits": "npm:inherits@^2.0.1",
-      "json": "github:systemjs/plugin-json@master",
-      "ieee754": "npm:ieee754@^1.1.1",
-      "base64-js": "npm:base64-js@0.0"
-    },
-    "npm:Base64@0.2.1": {},
-    "npm:inherits@2.0.1": {},
-    "npm:ieee754@1.1.4": {},
-    "npm:base64-js@0.0.7": {},
-    "jquery": "github:components/jquery@^2.1.1"
-  }
-});
-
-System.config({
-  "versions": {
-    "github:ded/reqwest": "1.1.2",
-    "npm:uri-templates": "0.1.5",
-    "github:jspm/nodelibs": "0.0.3",
-    "npm:Base64": "0.2.1",
-    "npm:inherits": "2.0.1",
-    "github:systemjs/plugin-json": "master",
-    "npm:ieee754": "1.1.4",
-    "npm:base64-js": "0.0.7",
-    "github:components/jquery": "2.1.1"
+    "npm:util@0.10.3": {
+      "inherits": "npm:inherits@2.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.0"
+    }
   }
 });
 
